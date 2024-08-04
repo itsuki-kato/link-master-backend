83973e5445e7c8b36ac94a1936c2330a5d72c429
TABLET-6FSNT7EB\gojin <larc.ituki@gmail.com>
2024-08-04 12:23:24 +0900

    prisma, graphql関連のセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from 'nestjs-prisma';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TopicModule } from './modules/topic/topic.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+      autoSchemaFile: './src/schema.graphql',
+    }),
+    PrismaModule.forRoot({ isGlobal: true }),
+    TopicModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/modules/topic/topic.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTopicInput } from './dto/create-topic.input';
+import { UpdateTopicInput } from './dto/update-topic.input';
+
+@Injectable()
+export class TopicService {
+  create(createTopicInput: CreateTopicInput) {
+    return 'This action adds a new topic';
+  }
+
+  findAll() {
+    return `This action returns all topic`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} topic`;
+  }
+
+  update(id: number, updateTopicInput: UpdateTopicInput) {
+    return `This action updates a #${id} topic`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} topic`;
+  }
+}
